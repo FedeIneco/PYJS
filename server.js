609f86de8d0ba80d73b6a9ce387f20438a3b9a9d
FedeIneco <federico.diaz@ineco.com>
2023-04-20 13:05:49 +0200

    servidor

--- server.js
+++ server.js
@@ -1,33 +1,43 @@
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
 const { exec } = require('child_process');
 
 const app = express();
-
+const upload = multer();
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(express.json());
+// app.get('/', async (req, res) => {
+//     res.status(200).send({
+//       message: 'Tamo en vivo!'
+//     })
+//   })
 
-app.get('/', async (req, res) => {
-    res.status(200).send({
-      message: 'Tamo en vivo!'
-    })
-  })
+app.post('/convert-to-xkt', upload.single('archivo'), (req, res) => {
+  const texto = req.body.texto;
+  const archivo = req.file;
 
-app.post('/convert-to-xkt', (req, res) => {
-// const nombre = req.body.nombre;
-// const datos = req.body.datos;
-console.log(req.body.datos);
-//   const command = `mi_comando --parametro1=${parametro1} --parametro2=${parametro2}`;
+console.log(texto);
+  
+// res.status(200).send({
+//  message:datos.name
+// })
+console.log('Texto:', texto);
+console.log('Archivo:', archivo);
 
-//   exec(command, (error, stdout, stderr) => {
-//     if (error) {
-//       console.error(`Error al ejecutar el comando: ${error}`);
-//       return res.status(500).json({ error: 'Error al ejecutar el comando' });
-//     }
-//     console.log(`Salida del comando: ${stdout}`);
-//     return res.status(200).json({ mensaje: 'Comando ejecutado correctamente' });
-  //});
+// Aquí se puede hacer lo que se necesite con el archivo y el string recibidos
+
+const command = `node createProject.js -p ${texto} -s ${archivo.originalname}`;
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error al ejecutar el comando: ${error}`);
+      return res.status(500).json({ error: 'Error al ejecutar el comando' });
+    }
+    console.log(`Salida del comando: ${stdout}`);
+    return res.status(200).json({ mensaje: 'Comando ejecutado correctamente' });
+  });
 });
 
 app.listen(8080, () => {
