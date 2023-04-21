58c8f0303cd92237a09bc2a61580d7942d5dbfa7
FedeIneco <federico.diaz@ineco.com>
2023-04-21 10:19:17 +0200

    subida multiples archivos

--- server.js
+++ server.js
@@ -1,35 +1,44 @@
 const express = require('express');
-const path = requiere('path');
+const path = require('path');
 const cors = require('cors');
 const multer = require('multer');
+const mimeType = require('mime-types');
 const { exec } = require('child_process');
 
 const app = express();
-const upload = multer();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: function(req, file, cb){
+    cb(null, file.originalname)
+  }
+});
+
+const upload = multer({
+  storage: storage
+}).array('archivo', 10);
+
 app.get('/', async (req, res) => {
-    res.status(200).send({
-      message: 'Tamo en vivo!'
-    })
-  })
+  res.status(200).send({
+    message: 'Tamo en vivo!'
+  });
+});
 
-app.post('/convert-to-xkt', upload.single('archivo'), (req, res) => {
-  const texto = req.body.texto;
-  const archivo = req.file;
+app.get('/convert-to-xkt', async (req, res) => {
+  res.status(200).send({
+    message: 'HOLA!'
+  });
+});
 
-console.log(texto);
-  
-// res.status(200).send({
-//  message:datos.name
-// })
-console.log('Texto:', texto);
-console.log('Archivo:', archivo);
+app.post('/convert-to-xkt', upload, (req, res) => {
+  const texto = req.body.texto;  
 
-// Aquí se puede hacer lo que se necesite con el archivo y el string recibidos
+  // Aquí se puede hacer lo que se necesite con el archivo y el string recibidos
 
-const command = `node createProject.js -p ${texto} -s ${archivo.originalname}` ;
+  const command = `node createProject.js -p ${texto} -s ./uploads/*.ifc`;
 
   exec(command, (error, stdout, stderr) => {
     if (error) {
