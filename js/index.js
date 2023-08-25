fc181109f2c2bac72e44bc12c42e4932037248e1
FedeIneco <federico.diaz@ineco.com>
2023-08-25 14:22:39 +0200

    a

--- js/index.js
+++ js/index.js
@@ -21,6 +21,10 @@ import {
   usoEspacios,
   obtenerOcupacion,
 } from "./excelJB.js";
+
+import { objBien, objMal, idsBien, idsMal } from "./excelpy.js";
+
+import { ids } from "./excelpy.js";
 let types = [];
 
 const formData = new FormData();
@@ -44,15 +48,23 @@ const barEspacios = document.getElementById("bar-espacios");
 const capturas = document.getElementById("capturas");
 const canvas = document.getElementById("myCanvas");
 const ocupacionesButton = document.getElementById("ocupacion");
+const checkBtn = document.getElementById("check-btn");
+const correctos = document.getElementById("correctos");
+const incorrectos = document.getElementById("incorrectos");
+const checkContainer = document.getElementById("checkContainer");
+const exportWrong = document.getElementById("exportWrong");
+const checkName = document.getElementById("checkName");
+const checkAllNamesButton = document.getElementById("checkAllNames");
+const namesDiv = document.getElementsByClassName("xeokit-form-check");
 
 form.style.display =
-  window.location.href === "https://xkt.onrender.com/" ? "block" : "none";
+  window.location.href === "http://localhost:3000/" ? "block" : "none";
 lista.style.display =
-  window.location.href === "https://xkt.onrender.com/" ? "flex" : "none";
+  window.location.href === "http://localhost:3000/" ? "flex" : "none";
 filtros.style.display =
-  window.location.href !== "https://xkt.onrender.com/" ? "flex" : "none";
-  graficsButton.style.display =
-  window.location.href !== "https://xkt.onrender.com/" ? "flex" : "none";
+  window.location.href !== "http://localhost:3000/" ? "flex" : "none";
+graficsButton.style.display =
+  window.location.href !== "http://localhost:3000/" ? "flex" : "none";
 boton.addEventListener("click", enviar);
 
 //Función que envía el nombre del proyecto y los archivos que quiere convertir a xkt al servidor
@@ -63,7 +75,7 @@ function enviar() {
     formData.append("archivo", archivos[i]);
   }
   formData.append("texto", texto);
-  fetch("https://xkt.onrender.com/api/convert-to-xkt", {
+  fetch("http://localhost:3000/api/convert-to-xkt", {
     method: "POST",
     body: formData,
   })
@@ -86,7 +98,7 @@ const listado = document.getElementById("menuListado");
 
 //Actualiza el listado de proyectos creaados una vez se ha convertido a xkt
 function archivosCreados() {
-  fetch("https://xkt.onrender.com/api/projects")
+  fetch("http://localhost:3000/api/projects")
     .then((response) => response.json())
     .then((data) => {
       listado.innerHTML = "";
@@ -98,7 +110,7 @@ function archivosCreados() {
         const li = document.createElement("li");
 
         // Configurar el texto del elemento li
-        li.innerHTML = `<a href="https://xkt.onrender.com/?projectId=${filename}">${cont} - ${filename}</a>`;
+        li.innerHTML = `<a href="http://localhost:3000/?projectId=${filename}">${cont} - ${filename}</a>`;
 
         // Agregar el elemento li a la lista
         listado.appendChild(li);
@@ -388,6 +400,7 @@ window.onload = function () {
       justiciaContainer.classList.toggle("ocultar");
     }
   });
+
   pieButton.addEventListener("click", () => {
     pieChart.classList.toggle("ocultar");
     if (!linearChart.classList.contains("ocultar")) {
@@ -400,6 +413,7 @@ window.onload = function () {
       justiciaContainer.classList.toggle("ocultar");
     }
   });
+
   linearButton.addEventListener("click", () => {
     linearChart.classList.toggle("ocultar");
     if (!barChart.classList.contains("ocultar")) {
@@ -412,18 +426,19 @@ window.onload = function () {
       justiciaContainer.classList.toggle("ocultar");
     }
   });
+
   const cerrarEspacios = document.getElementById("cerrarEspacios");
   const cerrarOcupaciones = document.getElementById("cerrarOcupaciones");
   const cerraEstados = document.getElementById("cerraEstados");
   const cerrarEstadosPlanta = document.getElementById("cerrarEstadosPlanta");
- 
+
   const espaciosContainer = document.getElementById("bar-espacios");
   cerrarEspacios.addEventListener("click", () => {
     if (!espaciosContainer.classList.contains("ocultar")) {
       espaciosContainer.classList.toggle("ocultar");
     }
   });
-  
+
   cerrarOcupaciones.addEventListener("click", () => {
     if (!pieChart.classList.contains("ocultar")) {
       pieChart.classList.toggle("ocultar");
@@ -439,7 +454,7 @@ window.onload = function () {
       linearChart.classList.toggle("ocultar");
     }
   });
-  
+
   floorsSelect.addEventListener("change", () => {
     const elementoSeleccionado = floorsSelect.value;
     let planta = "";
@@ -486,18 +501,18 @@ window.onload = function () {
           bimViewer.setAllObjectsVisible(false);
           bimViewer.setObjectsVisible(spacesids, true);
           bimViewer.setObjectsVisible(noSpacesids, false);
-          // contadorNotariado.forEach((elemento) => {
-          //   const objetoSeleccionado = bimViewer.viewer.scene.objects[elemento.globalID];
-          //   objetoSeleccionado.colorize = color3;
-          // });
-          // contadorJuridica.forEach((elemento) => {
-          //   const objetoSeleccionado = bimViewer.viewer.scene.objects[elemento.globalID];
-          //   objetoSeleccionado.colorize = color2;
-          // });
-          // contadorNacionalidad.forEach((elemento) => {
-          //   const objetoSeleccionado = bimViewer.viewer.scene.objects[elemento.globalID];
-          //   objetoSeleccionado.colorize = color;
-          // });
+          contadorNotariado.forEach((elemento) => {
+            const objetoSeleccionado = bimViewer.viewer.scene.objects[elemento.globalID];
+            objetoSeleccionado.colorize = color3;
+          });
+          contadorJuridica.forEach((elemento) => {
+            const objetoSeleccionado = bimViewer.viewer.scene.objects[elemento.globalID];
+            objetoSeleccionado.colorize = color2;
+          });
+          contadorNacionalidad.forEach((elemento) => {
+            const objetoSeleccionado = bimViewer.viewer.scene.objects[elemento.globalID];
+            objetoSeleccionado.colorize = color;
+          });
         });
         break;
       } else {
@@ -520,6 +535,10 @@ window.onload = function () {
     bimViewer.setObjectsVisible(ids, false);
     bimViewer.setObjectsVisible(techosIds, false);
     bimViewer.set3DEnabled(false);
+    for (let i = 0; i < ids.length; i++) {
+      bimViewer.setObjectsSelected(ids, true);
+      bimViewer.setObjectsVisible(ids, false);
+    }
   });
 
   // const checkIfcTypes = setInterval(() => {
@@ -607,7 +626,7 @@ window.onload = function () {
     formData.append("imagen", imagen);
 
     try {
-      const response = await fetch("https://xkt.onrender.com/guardar-imagen", {
+      const response = await fetch("http://localhost:3000/guardar-imagen", {
         method: "POST",
         body: formData,
       });
@@ -623,7 +642,113 @@ window.onload = function () {
     }
   });
 
-  window.bimViewer = bimViewer; // For debugging
-};
+  checkBtn.addEventListener("click", () => {
+    checkContainer.classList.toggle("ocultar");
+  });
 
-// import{Server as e,BIMViewer as t,LocaleService as l,addObserver as a,agregarObservador as o,array as s}from"../lib/xeokit-bim-viewer/xeokit-bim-viewer.es.js";import{datos as n,colorGrafica as i}from"./graficas.js";import{messages as r}from"../lib/xeokit-bim-viewer/messages.js";import{objetos as c,plantasUnicas as d,plantasFiltradas as b,obtenerSpaces as p,noObtenerSpaces as u,ceilings as g,falsosTechosP1 as h,obtenerEspaciosUso as f,usoEspacios as m,obtenerOcupacion as E}from"./excelJB.js";let types=[],formData=new FormData,boton=document.getElementById("boton"),form=document.getElementById("myForm"),lista=document.getElementById("listado"),datosInput=document.getElementById("fileInput"),barButton=document.getElementById("bar-btn"),pieButton=document.getElementById("pie-btn"),linearButton=document.getElementById("line-btn"),barChart=document.getElementById("bar"),pieChart=document.getElementById("pie"),linearChart=document.getElementById("linear"),floorsSelect=document.getElementById("floors"),espaciosButton=document.getElementById("espacios"),plantasButton=document.getElementById("plantas"),justiciaContainer=document.getElementById("pset"),barEspacios=document.getElementById("bar-espacios"),capturas=document.getElementById("capturas"),canvas=document.getElementById("myCanvas"),ocupacionesButton=document.getElementById("ocupacion");function enviar(){let e=document.getElementById("texto").value,t=document.getElementById("archivo").files;for(let l=0;l<t.length;l++)formData.append("archivo",t[l]);formData.append("texto",e),fetch("https://xkt.onrender.com/api/convert-to-xkt",{method:"POST",body:formData}).then(e=>{if(!e.ok)throw Error("Error en la petici\xf3n");return e.json()}).then(e=>{console.log(e),archivosCreados()}).catch(e=>{console.error(e)})}form.style.display="https://xkt.onrender.com/"===window.location.href?"block":"none",lista.style.display="https://xkt.onrender.com/"===window.location.href?"flex":"none",boton.addEventListener("click",enviar);let listado=document.getElementById("menuListado");function archivosCreados(){fetch("https://xkt.onrender.com/api/projects").then(e=>e.json()).then(e=>{listado.innerHTML="";let t=e.filenames;t.forEach(e=>{let t=document.createElement("li");t.innerHTML=`<a href="https://xkt.onrender.com/?projectId=${e}">${e}</a>`,listado.appendChild(t)})}).catch(e=>console.error(e))}archivosCreados();let todasTablaseditadas=document.createElement("table"),exportButton=document.getElementById("export-btn"),saveButton=document.getElementById("save-btn");saveButton.onclick=()=>{alert("Propiedad guardada con \xe9xito");let e=document.querySelectorAll(".xeokit-table");for(let t=0;t<e.length;t++)todasTablaseditadas.insertAdjacentHTML("beforeend",e[t].children[0].innerHTML)},exportButton.onclick=()=>{let e=XLSX.utils.table_to_book(todasTablaseditadas);XLSX.writeFile(e,"PropiedadesEditadas.xlsx")},window.onload=function(){let c=function e(){let t={};return window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,(e,l,a)=>{t[l]=a}),t}(),m=c.locale||"en",I=c.projectId;if(!I)return;let y=c.openExplorer;A("true"===y);let v="true"===c.enableEditModels,j=new e({dataDir:"./data"}),B=new t(j,{localeService:new l({messages:r,locale:m}),canvasElement:document.getElementById("myCanvas"),explorerElement:document.getElementById("myExplorer"),toolbarElement:document.getElementById("myToolbar"),inspectorElement:document.getElementById("myInspector"),navCubeCanvasElement:document.getElementById("myNavCubeCanvas"),busyModelBackdropElement:document.getElementById("myViewer"),enableEditModels:v});B.localeService.on("updated",()=>{let e=document.querySelectorAll(".xeokit-i18n");e.forEach(e=>{if(e.dataset.xeokitI18n&&(e.innerText=B.localeService.translate(e.dataset.xeokitI18n)),e.dataset.xeokitI18ntip){let t=B.localeService.translate(e.dataset.xeokitI18ntip);t&&(e.dataset.tippyContent=B.localeService.translate(e.dataset.xeokitI18ntip))}e.dataset.tippyContent&&(e._tippy?e._tippy.setContent(e.dataset.tippyContent):tippy(e,{appendTo:"parent",zIndex:1e6,allowHTML:!0}))})}),B.setConfigs({}),B.on("openExplorer",()=>{A(!0)}),B.on("openInspector",()=>{(function e(t){let l=document.getElementById("inspector_toggle");l&&(l.checked=t)})(!0)}),B.on("addModel",e=>{console.log("addModel: "+JSON.stringify(e,null,"	"))}),B.on("editModel",e=>{console.log("editModel: "+JSON.stringify(e,null,"	"))}),B.on("deleteModel",e=>{console.log("deleteModel: "+JSON.stringify(e,null,"	"))});let $=c.configs;if($){let C=$.split(",");for(let L=0,O=C.length;L<O;L++){let k=C[L],S=k.split(":"),_=S[0],x=S[1];B.setConfig(_,x)}}function A(e){let t=document.getElementById("explorer_toggle");t&&(t.checked=e)}B.loadProject(I,()=>{let e=c.modelId;e&&B.loadModel(e);let t=c.tab,l;t&&B.openTab(t),l="",window.setInterval(()=>{let e=window.location.hash;e!==l&&(function e(){let t=function e(){let t={},l,a=/\+/g,o=/([^&;=]+)=?([^&;]*)/g,s=function(e){return decodeURIComponent(e.replace(a," "))},n=window.location.hash.substring(1);for(;l=o.exec(n);)t[s(l[1])]=s(l[2]);return t}(),l=t.actions;if(!l)return;let a=l.split(",");if(0!==a.length)for(let o=0,s=a.length;o<s;o++){let n=a[o];switch(n){case"focusObject":let i=t.objectId;if(!i){console.error("Param expected for `focusObject` action: 'objectId'");break}B.setAllObjectsSelected(!1),B.setObjectsSelected([i],!0),B.flyToObject(i,()=>{});break;case"focusObjects":let r=t.objectIds;if(!r){console.error("Param expected for `focusObjects` action: 'objectIds'");break}let c=r.split(",");B.setAllObjectsSelected(!1),B.setObjectsSelected(c,!0),B.viewFitObjects(c,()=>{});break;case"clearFocusObjects":B.setAllObjectsSelected(!1),B.viewFitAll();break;case"openTab":let d=t.tabId;if(!d){console.error("Param expected for `openTab` action: 'tabId'");break}B.openTab(d);break;default:console.error("Action not supported: '"+n+"'")}}}(),l=e)},400)},e=>{console.error(e)});let w=e=>e;a(w),o(function e(t){let l=w(s);for(let a=0;a<t.length;a++)B.setAllObjectsSelected(!1),t[a].addEventListener("change",e=>{e.target.checked&&t[a].id==l[a].nombre?(B.setObjectsSelected(l[a].ids,!0),B.viewFitObjects(l[a].ids)):B.setObjectsSelected(l[a].ids,!1)})}),barButton.addEventListener("click",()=>{barChart.classList.toggle("ocultar"),pieChart.classList.contains("ocultar")||pieChart.classList.toggle("ocultar"),linearChart.classList.contains("ocultar")||linearChart.classList.toggle("ocultar"),justiciaContainer.classList.contains("ocultar")||justiciaContainer.classList.toggle("ocultar")}),pieButton.addEventListener("click",()=>{pieChart.classList.toggle("ocultar"),linearChart.classList.contains("ocultar")||linearChart.classList.toggle("ocultar"),barChart.classList.contains("ocultar")||barChart.classList.toggle("ocultar"),justiciaContainer.classList.contains("ocultar")||justiciaContainer.classList.toggle("ocultar")}),linearButton.addEventListener("click",()=>{linearChart.classList.toggle("ocultar"),barChart.classList.contains("ocultar")||barChart.classList.toggle("ocultar"),pieChart.classList.contains("ocultar")||pieChart.classList.toggle("ocultar"),justiciaContainer.classList.contains("ocultar")||justiciaContainer.classList.toggle("ocultar")});let D=document.getElementById("cerrar"),T=document.getElementById("bar-espacios");function M(){let e=n.map(e=>e.globalID),t=P(i);B.viewer.scene.selectedMaterial.edgeColor=t,B.viewer.scene.selectedMaterial.fillColor=t,B.setAllObjectsSelected(!1),B.setObjectsSelected(e,!0),B.viewFitObjects(e)}function V(e,t){let l=P(t);e.forEach(e=>{let t=B.viewer.scene.objects[e.globalID];t.colorize=l})}function F(e){let t=e.match(/\d+/g);if(!t||3!==t.length)return null;{let l=parseInt(t[0],10)/255,a=parseInt(t[1],10)/255,o=parseInt(t[2],10)/255;return[l,a,o]}}function P(e){let t=parseInt(e.slice(1,3),16)/255,l=parseInt(e.slice(3,5),16)/255,a=parseInt(e.slice(5,7),16)/255;return[t,l,a]}D.addEventListener("click",()=>{T.classList.contains("ocultar")||T.classList.toggle("ocultar")}),D.addEventListener("click",()=>{}),floorsSelect.addEventListener("change",()=>{let e=floorsSelect.value,t="";for(let l=0;l<d.length;l++){if(d[l]==e){t=b[l];let a=[];b[l].forEach(e=>{a.push(e.globalID)}),B.setAllObjectsSelected(!1),B.setAllObjectsVisible(!1),B.setObjectsVisible(a,!0),B.viewFitObjects(a);let o=[],s=[];espaciosButton.addEventListener("click",function(){f("S.G. NOTARIADO Y DE LOS REGISTROS"),f("D.G. SEG. JURIDICA Y FE PUBLICA"),f("S.G. NACIONALIDAD Y ESTADO CIVIL"),barEspacios.classList.remove("ocultar");let l=[],a=[];o=p(t,e),(s=u(t,e)).forEach(e=>{a.push(e.globalID)}),o.forEach(e=>{l.push(e.globalID)}),P("#37375C"),P("#8844B5"),P("#6B84E4"),B.setSpacesShown(!0),B.setAllObjectsSelected(!1),B.setAllObjectsVisible(!1),B.setObjectsVisible(l,!0),B.setObjectsVisible(a,!1)});break}B.setAllObjectsSelected(!1),B.setAllObjectsVisible(!0),B.viewFitAll()}}),plantasButton.addEventListener("click",function(){let e=[],t=[];h.forEach(e=>{t.push(e.globalID)}),g.forEach(t=>{e.push(t.globalID)}),B.setObjectsVisible(e,!1),B.setObjectsVisible(t,!1),B.set3DEnabled(!1)}),ocupacionesButton.addEventListener("click",()=>{let e=E("OCUPADO"),t=E("RESERVADO"),l=E("VACANTE");V(e,"#ff4d4d"),V(t,"#ffff4d"),V(l,"#70db70")}),barChart.addEventListener("click",M),pieChart.addEventListener("click",M),capturas.addEventListener("click",async e=>{let t=B.viewer.getSnapshot({format:"png",width:canvas.width,height:canvas.height,includeGizmos:!0}),l=new FormData;l.append("imagen",t);try{let a=await fetch("https://xkt.onrender.com/guardar-imagen",{method:"POST",body:l});a.ok?(console.log("Imagen guardada en el servidor"),window.location.href="captura"):console.error("Error al guardar la imagen en el servidor")}catch(o){console.error("Error al enviar la solicitud al servidor:",o)}}),window.bimViewer=B};
+  correctos.addEventListener("click", () => {
+    checkContainer.classList.add("ocultar");
+    bimViewer.viewer.scene.selectedMaterial.edgeColor = [0, 1, 0];
+    bimViewer.viewer.scene.selectedMaterial.fillColor = [0, 1, 0];
+    bimViewer.setAllObjectsSelected(false);
+    bimViewer.setObjectsSelected(idsBien, true);
+    bimViewer.viewFitObjects(idsBien);
+  });
+
+  incorrectos.addEventListener("click", () => {
+    checkContainer.classList.add("ocultar");
+    bimViewer.viewer.scene.selectedMaterial.edgeColor = [1, 0, 0];
+    bimViewer.viewer.scene.selectedMaterial.fillColor = [1, 0, 0];
+    bimViewer.setAllObjectsSelected(false);
+    bimViewer.setObjectsSelected(idsMal, true);
+    bimViewer.viewFitObjects(idsMal);
+  });
+
+  exportWrong.addEventListener("click", () => {
+    const allKeys = new Set();
+    objMal.forEach((item) => {
+      Object.keys(item).forEach((key) => allKeys.add(key));
+    });
+    const dataArray = objMal.map((item) => {
+      const row = [];
+      allKeys.forEach((key) => {
+        if (typeof item[key] === "object" && item[key] !== null) {
+          row.push(JSON.stringify(item[key]));
+        } else {
+          row.push(item[key]);
+        }
+      });
+      return row;
+    });
+
+    const workbook = XLSX.utils.book_new();
+
+    const worksheet = XLSX.utils.aoa_to_sheet([[...allKeys], ...dataArray]);
+
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Datos");
+
+    const excelBuffer = XLSX.write(workbook, {
+      type: "array",
+      bookType: "xlsx",
+    });
+
+    const blob = new Blob([excelBuffer], { type: "application/octet-stream" });
+
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "datos.xlsx";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+    Swal.fire({
+      title: "Éxito",
+      text: "Datos exportados correctamente",
+      icon: "success",
+      confirmButtonText: "Aceptar",
+    });
+  });
+
+  checkAllNamesButton.addEventListener("click", () => 
+  {
+    Swal.fire({
+      title: "Éxito",
+      text: "Datos exportados correctamente",
+      icon: "success",
+      confirmButtonText: "Aceptar",
+    });
+  })
+  checkName.addEventListener("click", checkProjectName);
+
+  function checkProjectName(nameProjects, regExp) {
+    let check = true;
+    for(let i = 0; i< namesDiv.length; i++){
+      const labelElements = namesDiv[i].querySelectorAll('label');
+      console.log(namesDiv[i]);
+      console.log(labelElements[0].innerHTML);
+      if(!(regExp.test(labelElements[0].innerHTML))){
+          check = false;
+      }
+      if(!check){
+        Swal.fire({
+          title: "Atención",
+          text: "Alguno de los nombres no ha cumplido con los estándares",
+          icon: "warning",
+          confirmButtonText: "Aceptar",
+        });  
+    }
+  }
+  Swal.fire({
+    title: "Información",
+    text: "Checkeo completado",
+    icon: "info",
+    confirmButtonText: "Aceptar",
+  });    
+}
+
+function checkProjectName(){
+
+}
+  window.bimViewer = bimViewer;
+};
